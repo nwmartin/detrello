515544b18af5efbb1af0a3f6235195946c3cde70
Nick Martin <martin.nick@gmail.com>
2012-12-03 22:31:33 -0500

    Getting all the lists on a board.

--- detrello.rb
+++ detrello.rb
@@ -1,17 +1,49 @@
 require 'yaml'
 require 'trollop'
+require 'trello'
 
 class Detrello
+	include Trello
+	include Trello::Authorization
 
 	def initialize(config)
 		@config = YAML.load_file(config)
-		puts @config.inspect
+
+		Trello::Authorization.const_set :AuthPolicy, OAuthPolicy
+		OAuthPolicy.consumer_credential = OAuthCredential.new @config["key"], @config["secret"]
+		OAuthPolicy.token = OAuthCredential.new @config["token"], nil
+	end
+
+	def deboard(board)
+		begin
+			board = Board.find(board)
+		rescue Exception => e
+			puts 'Board ' + board + ' is invalid. Try snagging the id from the board url.'
+			abort
+		end
+		unless board.has_lists?
+			puts 'Board ' + board + ' doesn\'t actually have any lists.'
+		end
+
+		board.lists.each do |list|
+			delist(list)
+		end
+
+	end
+
+	def delist(list)
+		puts list.inspect
+	end
+
+	def decard(card)
 	end
 
 end
 
 opts = Trollop::options do
 	opt :config, "Config file name", :type => :string, :required => true
+	opt :board, "Board name", :type => :string, :required => true
 end
 
 detrello = Detrello.new(opts[:config])
+puts detrello.deboard(opts[:board])
